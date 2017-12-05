b73f562a354f2a03c0cc5842a286a8f3f4141671
brandon chiang <yohunj@gmail.com>
2017-12-05 23:03:02 +0800

    * 實作一個清單頁面，顯示欄位有「客戶名稱、聯絡人數量、銀行帳戶數量」共三個欄位，此功能要在資料庫中建立一個檢視表，並且匯入 EDMX。 * 主選單要有連結可以直接連到三個主要功能的列表頁。

---
+++ Homework/Controllers/客戶帳戶聯絡人數量Controller.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Homework.Models;
+
+namespace Homework.Controllers
+{
+    public class 客戶帳戶聯絡人數量Controller : Controller
+    {
+        private 客戶Entities db = new 客戶Entities();
+
+        // GET: 客戶帳戶聯絡人數量
+        public ActionResult Index()
+        {
+            return View(db.客戶帳戶聯絡人數量.ToList());
+        }
+
+        // GET: 客戶帳戶聯絡人數量/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶帳戶聯絡人數量 客戶帳戶聯絡人數量 = db.客戶帳戶聯絡人數量.Find(id);
+            if (客戶帳戶聯絡人數量 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶帳戶聯絡人數量);
+        }
+
+        //// GET: 客戶帳戶聯絡人數量/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: 客戶帳戶聯絡人數量/Create
+        //// 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        //// 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "Id,客戶名稱,銀行帳戶數量,聯絡人數量")] 客戶帳戶聯絡人數量 客戶帳戶聯絡人數量)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.客戶帳戶聯絡人數量.Add(客戶帳戶聯絡人數量);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(客戶帳戶聯絡人數量);
+        //}
+
+        //// GET: 客戶帳戶聯絡人數量/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    客戶帳戶聯絡人數量 客戶帳戶聯絡人數量 = db.客戶帳戶聯絡人數量.Find(id);
+        //    if (客戶帳戶聯絡人數量 == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(客戶帳戶聯絡人數量);
+        //}
+
+        //// POST: 客戶帳戶聯絡人數量/Edit/5
+        //// 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        //// 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "Id,客戶名稱,銀行帳戶數量,聯絡人數量")] 客戶帳戶聯絡人數量 客戶帳戶聯絡人數量)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(客戶帳戶聯絡人數量).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(客戶帳戶聯絡人數量);
+        //}
+
+        // GET: 客戶帳戶聯絡人數量/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶帳戶聯絡人數量 客戶帳戶聯絡人數量 = db.客戶帳戶聯絡人數量.Find(id);
+            if (客戶帳戶聯絡人數量 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶帳戶聯絡人數量);
+        }
+
+        //// POST: 客戶帳戶聯絡人數量/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    客戶帳戶聯絡人數量 客戶帳戶聯絡人數量 = db.客戶帳戶聯絡人數量.Find(id);
+        //    db.客戶帳戶聯絡人數量.Remove(客戶帳戶聯絡人數量);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Homework/Models/客戶.Context.cs
+++ Homework/Models/客戶.Context.cs
@@ -28,7 +28,6 @@ namespace Homework.Models
         public virtual DbSet<客戶資料> 客戶資料 { get; set; }
         public virtual DbSet<客戶銀行資訊> 客戶銀行資訊 { get; set; }
         public virtual DbSet<客戶聯絡人> 客戶聯絡人 { get; set; }
-
-        public System.Data.Entity.DbSet<Homework.Models.客戶銀行資訊MetaData> 客戶銀行資訊MetaData { get; set; }
+        public virtual DbSet<客戶帳戶聯絡人數量> 客戶帳戶聯絡人數量 { get; set; }
     }
 }
